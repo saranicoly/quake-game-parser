34424d2145dcd28ac1ca85f31df33d9bc6329f15
Sara Lins <saranicoly0@gmail.com>
2022-01-07 23:21:02 -0300

    add output file

--- parser.py
+++ parser.py
@@ -1,15 +1,21 @@
+# open files
 log_file = open("qgames.log")
+output_file = open("output.txt", "w")
+
+# initialize variables
 players = []
 kills = {}
 count = 0
 
-# get the name of each player for each match
 for line in log_file:
+    if "InitGame" in line:
+        count+=1
+        output_file.write('"game_%d": {\n' % count)
+        # reinitialize variables
+        players = []
+        kills = {}
+
     if "Kill" in line:
         # get the killer --> can be improved using regex
         killer = line.split(":")[3].strip().split()[0]        
         # print(killer)
-
-    if "ShutdownGame" in line:
-        count+=1
-        print("game_%d" % count)
