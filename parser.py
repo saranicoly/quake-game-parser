432791ae8d15e604f52af3cc992c2cbb6f568e63
Sara Lins <saranicoly0@gmail.com>
2022-01-07 23:29:04 -0300

    add total kills

--- parser.py
+++ parser.py
@@ -2,20 +2,21 @@
 log_file = open("qgames.log")
 output_file = open("output.txt", "w")
 
-# initialize variables
-players = []
-kills = {}
 count = 0
+kills_count = 0
 
 for line in log_file:
     if "InitGame" in line:
         count+=1
         output_file.write('"game_%d": {\n' % count)
+        output_file.write('"total_kills": %d,\n' % kills_count)
         # reinitialize variables
+        kills_count = 0
         players = []
         kills = {}
 
     if "Kill" in line:
+        kills_count += 1
         # get the killer --> can be improved using regex
         killer = line.split(":")[3].strip().split()[0]        
         # print(killer)
